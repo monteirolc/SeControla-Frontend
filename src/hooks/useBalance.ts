67d05ca17687594efc4d191323e29a32db2f3676
Lucas M.C. <lucasm.castro47@gmail.com>
2025-09-01 10:49:26 -0300

    change usaBalance and service api to give backend informantions correctly

---
+++ src/hooks/useBalance.ts
@@ -0,0 +1,46 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { fetchBalance } from "@/services/api";
+import Cookies from "js-cookie";
+
+interface Balance {
+  id: string;
+  name: string;
+  created_at: string;
+  total_incomes: string;
+}
+
+export function useBalance(token: string) {
+  const [balance, setBalance] = useState<Balance[] | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function loadBalance() {
+      try {
+        setLoading(true);
+
+        let authToken = token;
+        if (!authToken) {
+          authToken = String(Cookies.get("access"))?.toString(); // nome do cookie que você salvou o JWT
+        }
+
+        const data = await fetchBalance(authToken);
+        console.log(data);
+        setBalance(data);
+      } catch (err: unknown) {
+        if (err instanceof Error) {
+          setError(err.message);
+        } else {
+          setError("Erro desconhecido");
+        }
+      } finally {
+        setLoading(false);
+      }
+    }
+    loadBalance();
+  }, [token]);
+
+  return { balance, loading, error };
+}
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,49 @@
-import axios from "axios";
 
-const api = axios.create({
-  baseURL: "http://localhost:8000/api/", // coloque o endpoint da sua API Django
-});
+const baseURL = "http://localhost:8000/api";
 
-export default api;
+export async function fetchLogin(username: string, password: string) {
+  try {
+    const res = await fetch(`${baseURL}/auth/login/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    });
+    const response = await res.json()
+    console.log(response)
+    if(!response) {
+      throw new Error("Login failed");
+    }
+    return response;
+
+  }
+  catch (error: unknown) {
+    console.error(error);
+    return null
+  }
+}
+
+
+export async function fetchBalance(token: string) {
+  console.log(token);
+  const bearer = `Bearer ${String(token).trim()}`;
+  try {
+    const response = await fetch(`${baseURL}/balance/`, {
+      method: "GET",
+      headers: {
+        "Authorization": bearer,
+        "Content-Type": "application/json",
+      },
+      // credentials: "include", // caso use cookies
+    });
+    
+    if (!response.ok) {
+      throw new Error(`Erro ao buscar balance: ${response.status}`);
+    }
+    return response.json();
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}

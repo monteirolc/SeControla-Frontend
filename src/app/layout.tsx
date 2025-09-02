cabf0ff74f7e7cdd2e24c3f6751b3e450b37ade7
Lucas M.C. <lucasm.castro47@gmail.com>
2025-09-02 07:51:39 -0300

    changed layout to use metadata and show project favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,16 @@ import "./globals.css";
 import { ThemeProvider } from "@/context/ThemeContext";
 import { AuthProvider } from "@/context/AuthContext";
 import Navbar from "@/components/layout/Navbar";
+import type { Metadata } from "next";
 import Cookies from "js-cookie";
+
+export const metadata: Metadata = {
+  title: "SeControla",
+  description: "App de controle financeiro",
+  icons: {
+    icon: "/assets/images/FaviconSeControla.svg", // caminho relativo à pasta /public
+  },
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   const themeCookie = Cookies.get("theme") as "light" | "dark" || 'light';

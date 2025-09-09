a27d5904ddff54daf41565a2b6b222640fe7a3e9
Lucas M.C. <lucasm.castro47@gmail.com>
2025-09-09 20:19:48 -0300

    chaged top spaces into dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -41,63 +41,66 @@ export default function DashboardPage() {
   }
 
   return (
-    <main className="flex items-center justify-center min-h-screen dark:bg-gray-950">
-      <div className="w-full max-w-2xl p-6 dark:bg-gray-700 bg-white rounded-2xl shadow-lg">
-        <h1 className="text-2xl font-bold text-center mb-6">Dashboard</h1>
-        {balance && balance.length > 0 ? (
-        <div className="space-y-4">
-          <strong className="p-1 justify-center">
-            {balanceResult(balance) > 0 ? (
-              <p className="text-green-500">{`Balanço: R$${(balanceResult(balance)).toFixed(2).replace(".", ",")}`}</p>
-            ) : (
-              <p className="text-red-500">{`Balanço: R$${balanceResult(balance).toFixed(2).replace(".", ",")}`}</p>
-            )}
-          </strong>
-          {balance.map(item => (
-            <div key={item.id} className="p-4 bg-gray-100 dark:bg-gray-800 rounded-lg shadow">
-              <p className="font-semibold">{item.name}</p>
-              <p className="text-sm text-gray-500 dark:text-gray-300">
-                Criado em: {new Date(item.created_at).toLocaleString("pt-BR")}
-              </p>
-              {(() => {
-                switch(item.account_type){
-                  case "i":
-                    return (
-                      <p className="text-sm text-green-500">
-                        Ganhos por carteira: R${Number(item.total_incomes).toFixed(2).replace(".", ",")}
-                      </p>
-                    )
-                  case "e":
-                    return (
-                      <p className="text-sm text-red-500">
-                        Gastos por carteira: R${Number(item.total_expenses).toFixed(2).replace(".", ",")}
-                      </p>
-                    )
-                  case "fe":
-                    return (
-                      <p className="text-sm text-red-500">
-                        Gasto fixo por carteira: R${Number(item.total_fixed_expenses).toFixed(2).replace(".", ",")}
-                      </p>
-                    )
-                  case "fi":
-                    return (
-                      <p className="text-sm text-blue-500">
-                        Ganho fixo por carteira: R${("0,00")}
-                      </p>
-                    )
-                }
-              })()}
-              <p className="text-[9px] text-gray-200">Proprietário: {item.owner}</p>
-              {accounts__(item.shared_accounts) === null ? (<p></p>) : (
-                <p className="text-[9px] text-gray-200">Conta compartilhada: { accounts__(item.shared_accounts) }</p>
+    <div className="dark:bg-gray-950 bg-white rounded-2xl shadow-lg">
+      <br></br>
+      <main className="flex items-center justify-center min-h-screen dark:bg-gray-950">
+        <div className="w-full max-w-2xl p-6 dark:bg-gray-700 bg-white rounded-2xl shadow-lg">
+          <h1 className="text-2xl font-bold text-center">Dashboard</h1>
+          {balance && balance.length > 0 ? (
+          <div className="space-y-2">
+            <strong>
+              {balanceResult(balance) > 0 ? (
+                <p className="text-center text-green-500">{`Balanço: R$${(balanceResult(balance)).toFixed(2).replace(".", ",")}`}</p>
+              ) : (
+                <p className="text-red-500">{`Balanço: R$${balanceResult(balance).toFixed(2).replace(".", ",")}`}</p>
               )}
-            </div>
-          ))}
+            </strong>
+            {balance.map(item => (
+              <div key={item.id} className="p-4 bg-gray-100 dark:bg-gray-800 rounded-lg shadow">
+                <p className="font-semibold">{item.name}</p>
+                <p className="text-sm text-gray-500 dark:text-gray-300">
+                  Criado em: {new Date(item.created_at).toLocaleString("pt-BR")}
+                </p>
+                {(() => {
+                  switch(item.account_type){
+                    case "i":
+                      return (
+                        <p className="text-sm text-green-500">
+                          Ganhos por carteira: R${Number(item.total_incomes).toFixed(2).replace(".", ",")}
+                        </p>
+                      )
+                    case "e":
+                      return (
+                        <p className="text-sm text-red-500">
+                          Gastos por carteira: R${Number(item.total_expenses).toFixed(2).replace(".", ",")}
+                        </p>
+                      )
+                    case "fe":
+                      return (
+                        <p className="text-sm text-red-500">
+                          Gasto fixo por carteira: R${Number(item.total_fixed_expenses).toFixed(2).replace(".", ",")}
+                        </p>
+                      )
+                    case "fi":
+                      return (
+                        <p className="text-sm text-blue-500">
+                          Ganho fixo por carteira: R${("0,00")}
+                        </p>
+                      )
+                  }
+                })()}
+                <p className="text-[9px] text-gray-200">Proprietário: {item.owner}</p>
+                {accounts__(item.shared_accounts) === null ? (<p></p>) : (
+                  <p className="text-[9px] text-gray-200">Conta compartilhada: { accounts__(item.shared_accounts) }</p>
+                )}
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p className="text-gray-500">Nenhum dado disponível</p>
+        )}
         </div>
-      ) : (
-        <p className="text-gray-500">Nenhum dado disponível</p>
-      )}
-      </div>
-    </main>
+      </main>
+    </div>
   );
 }

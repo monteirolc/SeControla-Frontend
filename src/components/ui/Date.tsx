ba6b5f738540aabbcae087c0c193d8f404a31808
Lucas M.C. <lucasm.castro47@gmail.com>
2025-09-17 10:03:31 -0300

    Created option into Date.tsx to the text stays up or left from the component

--- src/components/ui/Date.tsx
+++ src/components/ui/Date.tsx
@@ -6,7 +6,7 @@ type DateProps = {
   disabled?: boolean;
 };
 
-export default function DateUi({ type, text, value,  onChange, disabled = false }: DateProps) {
+export function DateUi({ type="date", text, value,  onChange, disabled = false }: DateProps) {
   return (
     <div className="flex items-center gap-2">
         <label className="block text-sm mb-1">{text}</label>
@@ -18,5 +18,25 @@ export default function DateUi({ type, text, value,  onChange, disabled = false 
           disabled={disabled}
           />
     </div>
+  );
+
+}
+
+export function DateUpText({ type, text, value,  onChange, disabled = false }: DateProps) {
+  return (
+    <>
+      <div className="flex items-center gap-2">
+        <label className="block text-sm mb-1">{text}</label>
+      </div>
+      <div className="flex items-center gap-2">
+        <input
+          type={type}
+          className="px-3 py-2 border rounded-lg dark:bg-gray-800"
+          onChange={(e) => onChange(e.target.value)}
+          value={value}
+          disabled={disabled}
+        />
+      </div>
+    </>
   );
 }
